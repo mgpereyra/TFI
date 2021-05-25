1ec2855447646bbd3a5b8ccc24fda39f7fc70e73
Leonel <leozapata21@hotmail.com>
2021-05-24 22:56:49 -0300

    finalizado Pantalla Login

--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/SplashScreenActivity.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/SplashScreenActivity.kt
@@ -10,7 +10,6 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
         sp_logo.alpha=0f
         sp_logo.animate().setDuration(1500).alpha(1f).withEndAction {
             val i= Intent(this, LoginActivity::class.java)
