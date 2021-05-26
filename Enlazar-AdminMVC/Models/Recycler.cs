39dd7bc0126ef00e77221de1c42ca8fd3076f17c
MarciaGiselle <margisetoledo@gmail.com>
2021-05-26 16:32:42 -0300

    Iniciando el proyecto - Rol Admin

---
+++ Enlazar-AdminMVC/Models/Recycler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Enlazar_AdminMVC.Models
+{
+    public class Recycler
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
