07d1e03c8a07f13d098ac461ce074e2d89964e74
Leonel <leozapata21@hotmail.com>
2021-05-27 15:53:56 -0300

    back btn en destalle info

--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/DetalleCardInfoActivity.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/DetalleCardInfoActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import ar.com.unlam.enlazar.R
+import kotlinx.android.synthetic.main.activity_detalle_card_info.*
 
 class DetalleCardInfoActivity : AppCompatActivity() {
     var id: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_card_info)
-
+        btnVolver.setOnClickListener { this@DetalleCardInfoActivity.finish() }
 
 
     }
