0966308201703b0e39cc103b88316210fcb0944e
martinmen <martinhmsuni@gmail.com>
2021-06-01 18:55:53 -0300

    Se agregan permisos
    Se agregan los permisos de internet y localizacion

--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/LoginActivity.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ar.com.unlam.enlazar.R
+import ar.com.unlam.enlazar.ui.recolector.DashboardRecolectorActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity()  {
@@ -12,7 +13,7 @@ class LoginActivity : AppCompatActivity()  {
         setContentView(R.layout.activity_login)
         btn_login.setOnClickListener {
             this@LoginActivity.finish()
-            startActivity(Intent(this,DashboardUserActivity::class.java))
+            startActivity(Intent(this,DashboardRecolectorActivity::class.java))
         }
         ir_crear_cuenta.setOnClickListener {
             this@LoginActivity.finish()
--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/recolector/MiRutaActivity.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/recolector/MiRutaActivity.kt
@@ -7,16 +7,15 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MiRutaActivity : AppCompatActivity() {
+    val database = Firebase.database
+    val myRef = database.getReference("message")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mi_ruta)
 
-        myRef.setValue("Hello, World!")
+        myRef.setValue("Hello, World!2")
     }
-
-
-    val database = Firebase.database
-    val myRef = database.getReference("message")
 
 
 }
