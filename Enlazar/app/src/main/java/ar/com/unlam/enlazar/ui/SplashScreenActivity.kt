85ed8fb23342488fd8e543b552a59c51fdc60aba
Leonel <leozapata21@hotmail.com>
2021-06-01 17:47:32 -0300

    card para encuetros pendientes

--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/SplashScreenActivity.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/SplashScreenActivity.kt
@@ -1,12 +1,12 @@
 package ar.com.unlam.enlazar.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import ar.com.unlam.enlazar.R
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
