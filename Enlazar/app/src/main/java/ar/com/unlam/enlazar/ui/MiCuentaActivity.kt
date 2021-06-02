6040dea18ad52af75750b865b1cb7db83bb6e764
Leonel <leozapata21@hotmail.com>
2021-06-02 00:10:17 -0300

    Screen mi cuenta ok

---
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/MiCuentaActivity.kt
@@ -0,0 +1,11 @@
+package ar.com.unlam.enlazar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MiCuentaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mi_cuenta)
+    }
+}
