0e85d9de813537e0491d42c1a7f9aeb5c57a652b
Leonel <leozapata21@hotmail.com>
2021-06-02 14:24:01 -0300

    mis canjes

---
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/MisCanjesActivity.kt
@@ -0,0 +1,11 @@
+package ar.com.unlam.enlazar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MisCanjesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mis_canjes)
+    }
+}
