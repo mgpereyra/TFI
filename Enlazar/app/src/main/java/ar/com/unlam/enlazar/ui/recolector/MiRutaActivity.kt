c51fbb45c9341ce952ad22bba29300d038eb661e
martinmen <martinhmsuni@gmail.com>
2021-06-02 14:37:36 -0300

    Se agrega map y trazar rutas
    Map
    Trazar rutas
    Activities de Recolector

--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/recolector/MiRutaActivity.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/recolector/MiRutaActivity.kt
@@ -1,20 +1,27 @@
 package ar.com.unlam.enlazar.ui.recolector
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ar.com.unlam.enlazar.R
+import ar.com.unlam.enlazar.ui.DetalleServicioActivity
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_mi_ruta.*
 
 class MiRutaActivity : AppCompatActivity() {
-    val database = Firebase.database
-    val myRef = database.getReference("message")
+    //val database = Firebase.database
+    //val myRef = database.getReference("message")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mi_ruta)
-
-        myRef.setValue("Hello, World!2")
+        buttonIrMapa.setOnClickListener{
+            val intent: Intent = Intent(this, MapRutaRecolectorActivity::class.java)
+            startActivity(intent)
+        }
+      //  myRef.setValue("Hello, World!2")
     }
 
 
