f0db9b8a8cb55d7a6f99bb94b8041b146252e5dd
martinmen <martinhmsuni@gmail.com>
2021-06-09 03:40:36 -0300

    Correcciones de estilos en recolector

---
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/model/Service.kt
@@ -0,0 +1,19 @@
+package ar.com.unlam.enlazar.model
+
+import java.util.*
+
+data class Service (
+        val address:String? = null,
+        val id:String? = null,
+        val latitud:String? = null,
+        val longitud:String? = null,
+        val envasesPlasticos:Int?? = null,
+        val envasesVidrio:Int?? = null,
+        val envasesCarton:Int?? = null,
+        val date: String? = null,
+        val time:String? = null,
+        val userId:String? = null,
+        val recolectorId:String? = null,
+        val estado:Int? = null,
+
+        )
