682669e9ea50b14b8a6b48954f4a3c9c71162b08
MarciaGiselle <margisetoledo@gmail.com>
2021-06-28 18:13:31 -0300

    Identando codigo

--- enlazar-admin/cliente/src/components/advice/Advice.jsx
+++ enlazar-admin/cliente/src/components/advice/Advice.jsx
@@ -1,54 +1,58 @@
-import React from 'react'
-import { useHistory} from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import {deleteAdviceAction, modifyAdvice} from '../../actions/adviceAction'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deleteAdviceAction, modifyAdvice } from "../../actions/adviceAction";
 
-const Advice = ({advice}) => {
-    const {tipe, title, content, id, img, uri} = advice;
+const Advice = ({ advice }) => {
+  const { tipe, title, content, id, img, uri } = advice;
 
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    //eliminar
-    const confirm = id => {
-        dispatch(deleteAdviceAction(id))
-    }
+  //eliminar
+  const confirm = (id) => {
+    dispatch(deleteAdviceAction(id));
+  };
 
-    //modificar
-    const confirmEdit = advice => {
-        dispatch( modifyAdvice(advice) )
-        history.push(`/edit-advice/${advice.id}`)
-    }
+  //modificar
+  const confirmEdit = (advice) => {
+    dispatch(modifyAdvice(advice));
+    history.push(`/edit-advice/${advice.id}`);
+  };
 
-    return (
-        <div className="col-lg-4">
-            <div className="card ">
-                <img className="card-img-top" src={uri} alt={img}></img>
-                    <div className="card-body">
-                    <p className="card-text"><small>{tipe}</small></p>
-                    <h3 className="card-title">{title}</h3>
-                    <p className="card-text cortar-texto">{content}</p>
-                    <div className='acciones'>
-                <button
-                    onClick={()=> confirmEdit(advice)}
-                    className='btn btn-primary mr-2 w-100'
-                ><i className="far fa-edit"></i>
-                  Editar  
-                </button>
-
-                <button
-                    onClick={()=> confirm(id)}
-                    className="btn btn-outline-primary w-100"
-                ><i className="far fa-trash-alt"></i>
-                  Eliminar  
-                </button>
-                
-            </div>    
-                </div>
-                <div className="card-footer"><small className="text-muted">{advice.id}</small></div>
-            </div>
+  return (
+    <div className="col-lg-4">
+      <div className="card ">
+        <img className="card-img-top" src={uri} alt={img}></img>
+        <div className="card-body">
+          <p className="card-text">
+            <small>{tipe}</small>
+          </p>
+          <h3 className="card-title">{title}</h3>
+          <p className="card-text cortar-texto">{content}</p>
+          <div className="acciones">
+            <button
+              onClick={() => confirm(id)}
+              className="btn btn-outline-primary w-100 btn-left"
+            >
+              <i className="far fa-trash-alt"></i>
+              Eliminar
+            </button>
+            <button
+              onClick={() => confirmEdit(advice)}
+              className="btn btn-primary w-100 btn-right"
+            >
+              <i className="far fa-edit"></i>
+              Editar
+            </button>
+          </div>
         </div>
-      );
-}
- 
+        <div className="card-footer">
+          <small className="text-muted">{advice.id}</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Advice;
