56325683a9aaf897225dc19823f9ff18ac50174f
Leonel <leozapata21@hotmail.com>
2021-06-29 18:31:29 -0300

    detalle de item a canjear y logica de canje

--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/adapter/ItemAdapter.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/adapter/ItemAdapter.kt
@@ -1,11 +1,14 @@
 package ar.com.unlam.enlazar.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ar.com.unlam.enlazar.R
 import ar.com.unlam.enlazar.model.Item
+import ar.com.unlam.enlazar.ui.vecino.DetalleCanjeItem
+import ar.com.unlam.enlazar.ui.vecino.MisServiciosDetalleActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.mis_canjes_card.view.*
 
@@ -13,7 +16,16 @@ class ItemAdapter: RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     var listItem = mutableListOf<Item>()
 
     class ItemViewHolder(view: View, var item: Item? = null) : RecyclerView.ViewHolder(view) {
+        init{
+            view.ver_item.setOnClickListener{
+                val intent = Intent(view.context, DetalleCanjeItem::class.java)
+                intent.putExtra(DetalleCanjeItem.ID_CANJE, item!!.id)
+                view.context.startActivity(intent)
 
+            }
+
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -30,9 +42,10 @@ class ItemAdapter: RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
         listItem[position].let {
             holder.itemView.cardInfoId.text = it.id
             Picasso.get()
-                .load(it.imageCode).error(R.drawable.error).into(holder.itemView.codigo)
+                .load(it.image).error(R.drawable.error).into(holder.itemView.codigo)
             holder.itemView.cardInfo_elemento_canje.text = it.title
             holder.itemView.cardInfo_costo.text = it.pointsCost.toString()
+            holder.itemView.cantidad_diponible.text=it.amount.toString()
 
 
         }
--- Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/vecino/DetalleCanjeItem.kt
+++ Enlazar/app/src/main/java/ar/com/unlam/enlazar/ui/vecino/DetalleCanjeItem.kt
@@ -2,10 +2,57 @@ package ar.com.unlam.enlazar.ui.vecino
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ar.com.unlam.enlazar.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_detalle_canje_item.*
+import kotlinx.android.synthetic.main.activity_mis_servicios_detalle.*
+import kotlinx.android.synthetic.main.activity_mis_servicios_detalle.cardInfoId
 
 class DetalleCanjeItem : AppCompatActivity() {
+    private val db = FirebaseDatabase.getInstance().reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle_canje_item)
+        if (intent.hasExtra(ID_CANJE)) {
+            verItem(intent.extras!!.getString(ID_CANJE, ""))
+        } else {
+            Toast.makeText(this@DetalleCanjeItem,
+                getString(R.string.valor_perdido), Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun verItem(id_item: String) {
+        db.child("Item").child(id_item).addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+               cupon_titulo_detalle.text=snapshot.child("title").value.toString()
+                costo_cupon_detalle.text=snapshot.child("pointCost").value.toString()
+                description_item.text=snapshot.child("description").value.toString()
+                cargarImagen(snapshot.child("image").value.toString())
+                cantidad_diponible.text=snapshot.child("amount").value.toString()
+                cardInfoId.text=snapshot.key
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun cargarImagen(img:String) {
+        Picasso.get()
+            .load(img)
+            .error(R.drawable.error)
+            .into(codigo)
+    }
+
+    companion object  {
+var ID_CANJE="ID"
     }
 }
